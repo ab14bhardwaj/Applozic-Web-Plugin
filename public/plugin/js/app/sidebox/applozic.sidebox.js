bef3e0f4741ce472672d62e5854f137439054192
Abhinav Bhardwaj <ab14bhardwaj@gmail.com>
2018-10-18 17:21:01 +0530

    KM-1584  Unable to set display name for context base chat in web plugin [FIX]
    1. While passing userName we were unable to set display name in context based chat previously, now we can.

--- public/plugin/js/app/sidebox/applozic.sidebox.js
+++ public/plugin/js/app/sidebox/applozic.sidebox.js
@@ -3561,8 +3561,12 @@ window.onload = function() {
                                                     }
                                                 }
                                                 if (userDetail.userName && !params.startTime) {
-                                                    var name = mckMessageLayout.getTabDisplayName(params.tabId, params.isGroup, userDetail.userName);
-                                                    $mck_tab_title.html(name);
+																										if (params.topicId && params.userName) {
+																											var name = mckMessageLayout.getTabDisplayName(params.tabId, params.isGroup, params.userName);
+																										} else {
+																											var name = mckMessageLayout.getTabDisplayName(params.tabId, params.isGroup, userDetail.userName);
+																										}
+																										$mck_tab_title.html(name);
                                                     $mck_tab_title.attr('title', name);
                                                 }
                                             }
